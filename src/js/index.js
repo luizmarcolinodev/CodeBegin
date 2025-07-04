f8823db40e5151d4638aa8616093cc6f8613d743
luizmarcolinodev <luizmarcolinodev@gmail.com>
2025-07-04 13:33:57 -0300

    chore(structure): criar pasta pages para organizar HTMLs
    Criada a pasta pages para centralizar e organizar todos os arquivos HTML do projeto. Refatorada a estrutura de diretórios para melhorar a manutenção.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 const openBtn = document.getElementById('openModal');
 const modal = document.getElementById('modal');
 
-
 openBtn.addEventListener("click", () => {
     modal.classList.add("open");
 });
